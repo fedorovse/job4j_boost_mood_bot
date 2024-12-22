63396814e7b9218a7cc5206e17b1dea0afdebeba
fedorovse <ingor-ru@mail.ru>
2024-12-23 00:33:07 +0700

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/BotCommandHandler.java
+++ src/main/java/ru/job4j/BotCommandHandler.java
@@ -1,4 +1,12 @@
 package ru.job4j;
 
+import org.springframework.stereotype.Service;
+import ru.job4j.content.Content;
+
+@Service
 public class BotCommandHandler {
+
+    public void receive(Content content) {
+        System.out.println(content);
+    }
 }
--- src/main/java/ru/job4j/service/TelegramBotService.java
+++ src/main/java/ru/job4j/service/TelegramBotService.java
@@ -1,4 +1,18 @@
 package ru.job4j.service;
 
+import org.springframework.stereotype.Service;
+import ru.job4j.BotCommandHandler;
+import ru.job4j.content.Content;
+
+@Service
 public class TelegramBotService {
+    private final BotCommandHandler handler;
+
+    public TelegramBotService(BotCommandHandler handler) {
+        this.handler = handler;
+    }
+
+    public void receive(Content content) {
+        handler.receive(content);
+    }
 }
